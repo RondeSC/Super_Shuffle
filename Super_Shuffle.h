8f50ef15b1b0ee6716ec775b6d45b0a1f5e2ec05
Ron <RondeSC@netscape.net>
2025-01-22 10:32:40 -0800

    Update MillerSuffle Algo to Jan2025 revision.

--- Super_Shuffle.h
+++ Super_Shuffle.h
@@ -26,7 +26,7 @@ unsigned int Super_Shuffle(unsigned int inx, unsigned int shuffleID, unsigned in
 	unsigned int si, hi, offset;
 	int halfSize = listSize / 2;  // for 1/2 the processing range
 
-	shuffleID += 131 * (inx / listSize);  // have inx overflows supported
+	shuffleID ^= (inx / listSize);  // have inx overflows supported
 	si = (inx % listSize);
 	if (si < halfSize) {
 		hi = si;
@@ -51,8 +51,8 @@ unsigned int MillerShuffleAlgo_e(unsigned int inx, unsigned int shuffleID, unsig
 	static unsigned int randR, halfN, rx, rkey;
 	const unsigned int p1 = 24317, p2 = 32141, p3 = 63629; // good for shuffling >60,000 indexes
 
-	shuffleID += 131 * (inx / listSize);  // have inx overflow effect the mix
-	si = (inx + shuffleID) % listSize;    // cut the deck
+	shuffleID ^= (inx / listSize);  // have inx overflow effect the mix
+	si = (shuffleID % listSize + inx) % listSize;    // cut the deck
 
 	randR = shuffleID;   //local randomizer
 	r1 = randR % p3;
@@ -67,7 +67,7 @@ unsigned int MillerShuffleAlgo_e(unsigned int inx, unsigned int shuffleID, unsig
 	if (si % 3 == 0)       si = (((unsigned long)(si / 3) * p1 + r1) % ((listSize + 2) / 3)) * 3; // spin multiples of 3 
 	if (si <= halfN) { si = (si + r3) % (halfN + 1); si = halfN - si; }  // improves large permu distro
 	if (si % 2 == 0)       si = (((unsigned long)(si / 2) * p2 + r2) % ((listSize + 1) / 2)) * 2; // spin multiples of 2 
-	if (si < halfN)    si = (si * p3 + r3) % halfN;
+	if (si < halfN)    si = ((unsigned long)si * p3 + r3) % halfN;
 
 	if ((si ^ rx) < listSize)   si ^= rx;			// flip some bits with Xor
 	si = ((unsigned long)si * p3 + r4) % listSize;  // a relatively prime gears churning operation
